89d90d55c83912436ebbbd994b8236fdf7336568
NurKeinNeid <mralexman3000@gmail.com>
2022-01-17 23:24:52 +0100

    Adapt for DerpFest
    * remove unused overlays
    * add back leaf icon shape (from: https://github.com/crdroidandroid/android_frameworks_base/commit/ad30bca7570998973fde94bbdc3ba3bdbc97168f)
    * replace hexagon icon shape (from: https://github.com/crdroidandroid/android_frameworks_base/commit/ad30bca7570998973fde94bbdc3ba3bdbc97168f)
    * add missing makefile call for square icon shape
    
    Change-Id: Ia8fe89069845578f7dfb64705586e3158f5f1326
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

---
+++ overlays.mk
@@ -0,0 +1,78 @@
+# Copyright (C) 2019 AospExtended ROM
+# Copyright (C) 2021 DerpFest
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Fonts
+$(call inherit-product-if-exists, external/google-fonts/arbutus-slab/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/arvo/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/source-sans-pro/fonts.mk)
+
+LOCAL_PATH := vendor/overlay/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+# Font Overalys
+PRODUCT_PACKAGES += \
+    FontArbutusSourceOverlay \
+    FontArvoLatoOverlay \
+    FontGoogleSansOverlay \
+    FontRubikRubikOverlay \
+    FontAdamCGinoraOverlay \
+    FontBigNoodleOverlay \
+    FontBikoHankenOverlay \
+    FontMittelschriftOverlay \
+    FontPisselOverlay \
+    FontReemKufiOverlay \
+    FontRobotoOverlay \
+    FontAclonicaSourceOverlay \
+    FontAmaranteSourceOverlay \
+    FontBariolSourceOverlay \
+    FontCagliostroSourceOverlay \
+    FontComfortaaSourceOverlay \
+    FontComicSansSourceOverlay \
+    FontCoolstorySourceOverlay \
+    FontLGSmartGothicSourceOverlay \
+    FontMontserratSourceOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateSource \
+    FontRosemarySourceOverlay \
+    FontSamsungOneSourceOverlay \
+    FontSonySketchSourceOverlay \
+    FontSurferSourceOverlay
+
+# Icon shapes
+PRODUCT_PACKAGES += \
+    IconShapeFlowerOverlay \
+    IconShapeHeartOverlay \
+    IconShapeHexagonOverlay \
+    IconShapeLeafOverlay \
+    IconShapeCloudyOverlay \
+    IconShapeCylindricalOverlay \
+    IconShapeStretchedOverlay \
+    IconShapePebbleOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay \
+    IconShapeRoundedHexagonOverlay
+
+# Themes
+PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay
